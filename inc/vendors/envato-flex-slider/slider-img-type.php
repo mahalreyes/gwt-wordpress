0a81593f2c11cb2b0b88d5594d16f04cc14bf2cf
Voj Jeturian <jeturian@gmail.com>
2017-10-03 10:05:46 +0800

    removed field-slider-link file, moved to slider-img-type, rename food and carbs into slider-link

--- inc/vendors/envato-flex-slider/slider-img-type.php
+++ inc/vendors/envato-flex-slider/slider-img-type.php
@@ -28,3 +28,62 @@ function efs_disable_editor() {
     remove_post_type_support( CPT_TYPE, 'editor' );
 }
 add_action('init', 'efs_disable_editor');
+
+/**
+ * Add meta box
+ *
+ * @param post $post The post object
+ * @link https://codex.wordpress.org/Plugin_API/Action_Reference/add_meta_boxes
+ */
+function slider_link_add_meta_boxes( $post ){
+  add_meta_box( 'slider_link_meta_box', __( 'Slider URL', 'efs_slider' ), 'slider_link_build_meta_box', CPT_TYPE, 'normal', 'low' );
+}
+add_action( 'add_meta_boxes_slider-image', 'slider_link_add_meta_boxes' );
+
+/**
+ * Build custom field meta box
+ *
+ * @param post $post The post object
+ */
+function slider_link_build_meta_box( $post ){
+  // make sure the form request comes from WordPress
+  wp_nonce_field( basename( __FILE__ ), 'slider_link_meta_box_nonce' );
+
+  // retrieve the _slider_link current value
+  $slider_link = get_post_meta( $post->ID, '_slider_link', true );
+  ?>
+  <div class='inside'>
+    <p><span class="field-prefix">http://</span>
+      <input type="text" name="slider_link" value="<?php echo $slider_link; ?>" /> 
+    </p>
+
+  </div>
+  <?php
+}
+
+/**
+ * Store custom field meta box data
+ *
+ * @param int $post_id The post ID.
+ * @link https://codex.wordpress.org/Plugin_API/Action_Reference/save_post
+ */
+function slider_link_save_meta_box_data( $post_id ){
+  // verify meta box nonce
+  if ( !isset( $_POST['slider_link_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['slider_link_meta_box_nonce'], basename( __FILE__ ) ) ){
+    return;
+  }
+  // return if autosave
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+    return;
+  }
+  // Check the user's permissions.
+  if ( ! current_user_can( 'edit_post', $post_id ) ){
+    return;
+  }
+
+  if ( isset( $_REQUEST['slider_link'] ) ) {
+    update_post_meta( $post_id, '_slider_link', sanitize_text_field( $_POST['slider_link'] ) );
+  }
+}
+add_action( 'save_post_slider-image', 'slider_link_save_meta_box_data' );
+
